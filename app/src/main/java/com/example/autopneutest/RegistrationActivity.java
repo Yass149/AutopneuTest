60592b9478f997f1045b0a254d7e0a60e7288943
Yass149 <48418035+Yass149@users.noreply.github.com>
2023-12-01 13:44:10 +0100

    Update

--- app/src/main/java/com/example/autopneutest/RegistrationActivity.java
+++ app/src/main/java/com/example/autopneutest/RegistrationActivity.java
@@ -34,7 +34,7 @@ public class RegistrationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration);
+
         adresse_mail = findViewById(R.id.adresse_email);
         password = findViewById(R.id.password);
         first_name = findViewById(R.id.first_name);
