240cfdf567bff6730cb6d4cb08303b7ea9985da7
Yass149 <48418035+Yass149@users.noreply.github.com>
2023-12-01 13:11:30 +0100

    Update

--- app/src/main/java/com/example/autopneutest/MainActivity.java
+++ app/src/main/java/com/example/autopneutest/MainActivity.java
@@ -36,7 +36,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this , RegistrationActivity.class));
-                finish();
             }
         });
 
