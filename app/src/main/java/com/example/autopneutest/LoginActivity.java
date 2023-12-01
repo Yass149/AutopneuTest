3bf4de97e4d1fe8394083b02375961e9050ee9be
Yass149 <48418035+Yass149@users.noreply.github.com>
2023-12-01 21:18:52 +0100

    Update

--- app/src/main/java/com/example/autopneutest/LoginActivity.java
+++ app/src/main/java/com/example/autopneutest/LoginActivity.java
@@ -3,15 +3,54 @@ package com.example.autopneutest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
+    private EditText adresse_mail;
+    private EditText password;
+    private Button login;
+private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseApp.initializeApp(this);
         setContentView(R.layout.activity_login_customer);
+
+        adresse_mail = findViewById(R.id.adresse_mail);
+        password = findViewById(R.id.password);
+        login = findViewById(R.id.login);
+
+        auth = FirebaseAuth.getInstance();
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txt_email = adresse_mail.getText().toString();
+                String txt_password = password.getText().toString();
+                loginUser(txt_email, txt_password);
+            }
+        });
+    }
+
+    private void loginUser(String adresse_mail, String password) {
+        auth.signInWithEmailAndPassword(adresse_mail, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Toast.makeText(LoginActivity.this, "login succesful", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(LoginActivity.this , MainActivity.class));
+                finish();
+            }
+        });
     }
 
     public void register(View view) {
--- app/src/main/java/com/example/autopneutest/RegistrationActivity.java
+++ app/src/main/java/com/example/autopneutest/RegistrationActivity.java
@@ -12,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -33,6 +34,7 @@ public class RegistrationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseApp.initializeApp(this);
         setContentView(R.layout.activity_registration);
 
         first_name = findViewById(R.id.first_name);
