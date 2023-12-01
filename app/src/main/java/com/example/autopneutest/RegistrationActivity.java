a76506f2de985f6519389b2bc368d9cdabc930fc
Yass149 <48418035+Yass149@users.noreply.github.com>
2023-12-01 15:50:59 +0100

    Update

--- app/src/main/java/com/example/autopneutest/RegistrationActivity.java
+++ app/src/main/java/com/example/autopneutest/RegistrationActivity.java
@@ -18,13 +18,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
 public class RegistrationActivity extends AppCompatActivity {
-    private EditText first_name;
-    private EditText last_name;
     private EditText password;
-    private EditText username;
     private EditText adresse_mail;
-    private EditText adresse;
-    private EditText phone_number;
     private Button register;
     private Button d_ja_inscrit;
     private Button se_connecter;
@@ -35,13 +30,8 @@ public class RegistrationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
-        first_name = findViewById(R.id.first_name);
-        last_name = findViewById(R.id.last_name);
-        username = findViewById(R.id.username);
         adresse_mail = findViewById(R.id.adresse_mail);
         password = findViewById(R.id.password);
-        phone_number = findViewById(R.id.phone_number);
-        adresse = findViewById(R.id.adresse);
         register = findViewById(R.id.register);
         d_ja_inscrit = findViewById(R.id.d_ja_inscrit);
         se_connecter = findViewById(R.id.login);
@@ -53,28 +43,20 @@ public class RegistrationActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String txt_adresse_mail = adresse_mail.getText().toString();
                 String txt_password = password.getText().toString();
-                String txt_first_name = first_name.getText().toString();
-                String txt_last_name = last_name.getText().toString();
-                String txt_username = username.getText().toString();
-                String txt_adresse = adresse.getText().toString();
-                String txt_phone_number = phone_number.getText().toString();
 
                 //if empty mechanism
-                if (TextUtils.isEmpty(txt_adresse_mail) || TextUtils.isEmpty(txt_password) || TextUtils.isEmpty(txt_first_name)
-                        || TextUtils.isEmpty(txt_last_name) || TextUtils.isEmpty(txt_phone_number) || TextUtils.isEmpty(txt_username)
-                        || TextUtils.isEmpty(txt_adresse)){
+                if (TextUtils.isEmpty(txt_adresse_mail) || TextUtils.isEmpty(txt_password)){
                     Toast.makeText(RegistrationActivity.this, "certains champs sont vides!", Toast.LENGTH_SHORT).show();
                 } else if (txt_password.length() < 6){
                     Toast.makeText(RegistrationActivity.this, "Le mot de passe est trop court!", Toast.LENGTH_SHORT).show();
                 } else {
-                    registerUser(txt_adresse_mail, txt_password, txt_first_name, txt_last_name, txt_adresse,txt_phone_number, txt_username);
+                    registerUser(txt_adresse_mail, txt_password);
                 }
             }
         });
     }
 
-    private void registerUser(String adresse_mail, String password, String first_name,
-                              String last_name, String adresse, String phone_number, String username) {
+    private void registerUser(String adresse_mail, String password) {
         auth.createUserWithEmailAndPassword(adresse_mail, password).addOnCompleteListener(RegistrationActivity.this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
