e0837508826ef7062b7dd659f9deae365245b8a0
Yass149 <48418035+Yass149@users.noreply.github.com>
2023-12-04 13:30:44 +0100

    Update

--- app/src/main/java/com/example/autopneutest/MainActivity.java
+++ app/src/main/java/com/example/autopneutest/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.autopneutest;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -21,29 +22,20 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
-    FirebaseAuth auth;
-    Button button;
-    TextView textView;
-    FirebaseUser user;
+    private FirebaseAuth auth;
+    private Button button;
+    private TextView textView;
+    private FirebaseUser user;
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
 
-   /* public void disconnect(View view) {
-
-
-
+    public void disconnect(View view) {
         Intent intent = new Intent(this, WelcomeActivity.class);
-
-
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-
         startActivity(intent);
-
-
         finish();
-    }*/
-
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,39 +44,50 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        auth =FirebaseAuth.getInstance();
+        auth = FirebaseAuth.getInstance();
+
+        // Find the button and text view by their IDs
         button = findViewById(R.id.disbtn);
         textView = findViewById(R.id.user_info);
-        user = auth.getCurrentUser();
-        if (user == null)
-        {
-            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-            startActivity(intent);
-            finish();
-        }else {
-            textView.setText(user.getEmail());
+
+        // Check if the button and text view are not null before using them
+        if (button != null) {
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    FirebaseAuth.getInstance().signOut();
+                    Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            });
+        } else {
+            Log.e("MainActivity", "Button 'disbtn' not found in the layout");
         }
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
+        if (textView != null) {
+            user = auth.getCurrentUser();
+            if (user == null) {
                 Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
                 startActivity(intent);
                 finish();
-
+            } else {
+                textView.setText(user.getEmail());
             }
-        });
+        } else {
+            Log.e("MainActivity", "TextView 'user_info' not found in the layout");
+        }
+
         setSupportActionBar(binding.appBarMain.toolbar);
 
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_catalog, R.id.nav_my_cart)
                 .setOpenableLayout(drawer)
                 .build();
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
@@ -92,7 +95,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
