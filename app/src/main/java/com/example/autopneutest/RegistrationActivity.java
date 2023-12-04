71368afac7f4d3dc5ab940d5a487044beb8bd896
Yass149 <48418035+Yass149@users.noreply.github.com>
2023-12-04 12:27:56 +0100

    Update

--- app/src/main/java/com/example/autopneutest/RegistrationActivity.java
+++ app/src/main/java/com/example/autopneutest/RegistrationActivity.java
@@ -42,7 +42,7 @@ public class RegistrationActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         editTextemail = findViewById(R.id.editText3);
         editTextpassword = findViewById(R.id.editText2);
-        buttonReg = findViewById(R.id.button);
+        buttonReg = findViewById(R.id.mbutton);
         progressBar = findViewById(R.id.progressBar);
 
         buttonReg.setOnClickListener(new View.OnClickListener() {
