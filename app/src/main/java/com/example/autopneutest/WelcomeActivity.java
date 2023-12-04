0b1764f1333fe1a274a9a60e63dcfe080f6bf427
Yass149 <48418035+Yass149@users.noreply.github.com>
2023-12-04 14:05:16 +0100

    Update

--- app/src/main/java/com/example/autopneutest/WelcomeActivity.java
+++ app/src/main/java/com/example/autopneutest/WelcomeActivity.java
@@ -8,15 +8,32 @@ import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class WelcomeActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Hide the title and set the window to full screen
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_welcome);
+
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance();
+
+        // Check if the user is already signed in
+        FirebaseUser user = auth.getCurrentUser();
+        if (user != null) {
+            // If the user is already signed in, sign them out
+            auth.signOut();
+        }
     }
 
     public void register(View view) {
@@ -24,8 +41,6 @@ public class WelcomeActivity extends AppCompatActivity {
     }
 
     public void login(View view) {
-
         startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
-
     }
 }
