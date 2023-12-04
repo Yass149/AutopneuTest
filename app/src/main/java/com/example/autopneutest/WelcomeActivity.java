282fe1961fd5da57ea855b9a590788bd7984e892
Yass149 <48418035+Yass149@users.noreply.github.com>
2023-12-04 12:17:55 +0100

    Update

--- app/src/main/java/com/example/autopneutest/WelcomeActivity.java
+++ app/src/main/java/com/example/autopneutest/WelcomeActivity.java
@@ -2,20 +2,14 @@ package com.example.autopneutest;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.textfield.TextInputEditText;
-
 public class WelcomeActivity extends AppCompatActivity {
-    Button buttonReg;
-    TextInputEditText editTextemail, editTextpassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,14 +17,6 @@ public class WelcomeActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_welcome);
-
-        buttonReg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String editText3, editText2;
-                editText3 = String.valueOf(editTextemail.getText());
-                editText2 = String.valueOf(editTextpassword.getText());
-
     }
 
     public void register(View view) {
