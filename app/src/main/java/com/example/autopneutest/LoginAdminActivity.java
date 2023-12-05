329fd03d3a378a7c61a26b97f0d07bb39d10bd23
Yass149 <48418035+Yass149@users.noreply.github.com>
2023-12-05 23:16:17 +0100

    Update

--- app/src/main/java/com/example/autopneutest/LoginAdminActivity.java
+++ app/src/main/java/com/example/autopneutest/LoginAdminActivity.java
@@ -3,14 +3,21 @@ package com.example.autopneutest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
 
 public class LoginAdminActivity extends AppCompatActivity {
 
@@ -35,10 +42,33 @@ public class LoginAdminActivity extends AppCompatActivity {
         buttonLog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Your existing code for login
-            }
-        });
+                progressBar.setVisibility(View.VISIBLE);
+                String adminEmail = String.valueOf(editTextAdminEmail.getText());
+                String adminPassword = String.valueOf(editTextAdminPassword.getText());
 
+                if (TextUtils.isEmpty(adminEmail) || TextUtils.isEmpty(adminPassword)) {
+                    Toast.makeText(LoginAdminActivity.this, "Enter email and password", Toast.LENGTH_SHORT).show();
+                    progressBar.setVisibility(View.GONE);
+                    return;
+                }
+
+                mAuth.signInWithEmailAndPassword(adminEmail, adminPassword)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressBar.setVisibility(View.GONE);
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(LoginAdminActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(LoginAdminActivity.this, MainAdminActivity.class); // Update with the correct activity class
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    Toast.makeText(LoginAdminActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+                                     });
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -46,7 +76,7 @@ public class LoginAdminActivity extends AppCompatActivity {
                 Intent intent = new Intent(LoginAdminActivity.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
-            }
-        });
-    }
-}
+                }
+            });
+
+    }  }
